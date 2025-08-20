0a0ed6dc27a5fbc482b1695fcc207c12f00c83f0
Liên Kết Số <lienketso@Liens-MacBook-Pro.local>
2025-08-20 17:14:40 +0700

    update code 2

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -30,7 +30,8 @@ class Team extends Model
 
     public function players(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'team_players', 'team_id', 'user_id');
+        return $this->belongsToMany(User::class, 'team_players', 'team_id', 'user_id')
+            ->select(['users.id', 'users.name', 'users.avatar', 'users.phone']);
     }
 
     // Scopes
